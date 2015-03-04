d2df33bb15af0ee25cff7537a2415f7797b9739d
matthieuP <matthieu.posnic@hotmail.fr>
2015-03-04 16:36:21 +0100

    install bootstrap

--- wp-content/themes/wine/header.php
+++ wp-content/themes/wine/header.php
@@ -2,8 +2,14 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/bootstrap.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /style/bootstrap.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /style/main.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /js/bootstrap.css" type="text/css" media="screen" />
 </head>
 <body>
 	
+<header>
+	
+	<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="logo">
+
+</header>
