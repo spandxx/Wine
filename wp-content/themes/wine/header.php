7fd4cd07a81a1917a3d07b16a5080e8357d38538
matthieuP <matthieu.posnic@hotmail.fr>
2015-03-04 16:17:19 +0100

    install bootstrap

---
+++ wp-content/themes/wine/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/bootstrap.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /js/bootstrap.css" type="text/css" media="screen" />
+</head>
+<body>
+	
