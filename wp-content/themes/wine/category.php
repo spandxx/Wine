13ed446684a8c43198a45921775a8d96e15b663f
Popone <Azn_lao_viet_style@hotmail.com>
2015-03-18 15:49:20 +0100

    modif

--- wp-content/themes/wine/category.php
+++ wp-content/themes/wine/category.php
@@ -3,9 +3,23 @@
  	<div id="test">
 	</div>
 	<!-- POUR AFFICHER LES NOMS DES ARTICLES DANS LA CATEGORIE -->
+	<section id="definition">
+    <div class="container">
+      <div class="row">
+       <div class="col-sm-7 blog-main">
+
+
+            
+            	<img class="img-responsive pub" src="<?php bloginfo('template_directory'); ?> /img/calque2.png"></a>
+            
+            </div>
+          
+
+    <div class="col-sm-2 col-sm-offset-2 blog-sidebar">     	
 	<?php
 
 		while ( have_posts() ) : the_post();
+
 			echo '<li>';
 			the_title();
 			echo '</li>';
@@ -14,5 +28,9 @@
 		wp_reset_query();
 		 
 	?>
+	</div>
+	  </div>
+            </div>
+        </section>
 
 <?php get_footer(); ?>
